675c3e588d15b41eb76e6920989568771a69ef0f
Saidur289 <saidur.riaz1@gmailcomm>
2024-12-25 18:46:04 +0600

    added some style

--- src/Pages/OurRevenue.jsx
+++ src/Pages/OurRevenue.jsx
@@ -2,15 +2,7 @@
 import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
 
 const OurRevenue = ({cars}) => {
-    // const bookingRevenueData = [
-    //     { month: "January", revenue: 5000 },
-    //     { month: "February", revenue: 7500 },
-    //     { month: "March", revenue: 6200 },
-    //     { month: "April", revenue: 8300 },
-    //     { month: "May", revenue: 9100 },
-    //   ];
-      
-    
+        
     return (
         <div className='container mx-auto pt-5'>
             <h1 className='text-primary text-3xl  text-center'>Trending Revenue</h1>
